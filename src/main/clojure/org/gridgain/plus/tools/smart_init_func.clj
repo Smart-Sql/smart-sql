994eb82145496391422f69ca6c7470339da7132c
chenfei <monk0123456@126.com>
2022-10-20 23:16:14 +0800

    2022-8-17

--- src/main/clojure/org/gridgain/plus/tools/smart_init_func.clj
+++ src/main/clojure/org/gridgain/plus/tools/smart_init_func.clj
@@ -46,43 +46,46 @@
         :main false
         ))
 
-(defn has_user_token_type [ignite group_id c_user_token_type_f1700]
-    (let [user_token_type (MyVar. (MyConvertUtil/ConvertToString c_user_token_type_f1700)) flag (MyVar. false) lst (MyVar. (my-lexical/to_arryList ["ddl" "dml" "all"]))]
+; 判断是否存在 DDL, DML, ALL 这三个字符
+(defn has_user_token_type [ignite group_id user_token_type]
+    (let [flag (MyVar. false) lst (MyVar. (my-lexical/to_arryList ["ddl" "dml" "all"]))]
         (do
             (cond (my-lexical/is-contains? (my-lexical/get-value lst) (str/lower-case (my-lexical/get-value user_token_type))) (.setVar flag true))
             (my-lexical/get-value flag))))
 
 
-(defn get_user_group [ignite group_id c_user_token_f1722]
-    (let [user_token (MyVar. (MyConvertUtil/ConvertToString c_user_token_f1722))]
-        (let [vs (MyVar. (my-lexical/no-sql-get-vs ignite group_id (doto (Hashtable.) (.put "table_name" "user_group_cache") (.put "key" (my-lexical/get-value user_token)))))]
-            (cond (my-lexical/not-empty? (my-lexical/get-value vs)) (my-lexical/get-value vs)
-                  :else (let [rs (MyVar. (my-smart-db/query_sql ignite group_id "select g.id, g.data_set_name, g.group_type from my_users_group as g where g.user_token = ?" [(my-lexical/to_arryList [(my-lexical/get-value user_token)])])) result (MyVar. )]
-                            (do
-                                (cond (my-lexical/my-is-iter? rs) (try
-                                                                      (loop [M-F-v1725-I-Q1726-c-Y (my-lexical/get-my-iter rs)]
-                                                                          (if (.hasNext M-F-v1725-I-Q1726-c-Y)
-                                                                              (let [r (.next M-F-v1725-I-Q1726-c-Y)]
-                                                                                  (my-lexical/no-sql-insert ignite group_id (doto (Hashtable.) (.put "table_name" "user_group_cache") (.put "key" (my-lexical/get-value user_token)) (.put "value" r)))
-                                                                                  (.setVar result r)
-                                                                                  (recur M-F-v1725-I-Q1726-c-Y))))
-                                                                      (catch Exception e
-                                                                          (if-not (= (.getMessage e) "my-break")
-                                                                              (throw e))))
-                                      (my-lexical/my-is-seq? rs) (try
-                                                                     (doseq [r (my-lexical/get-my-seq rs)]
-                                                                         (my-lexical/no-sql-insert ignite group_id (doto (Hashtable.) (.put "table_name" "user_group_cache") (.put "key" (my-lexical/get-value user_token)) (.put "value" r)))
-                                                                         (.setVar result r))
-                                                                     (catch Exception e
-                                                                         (if-not (= (.getMessage e) "my-break")
-                                                                             (throw e))))
-                                      :else
-                                      (throw (Exception. "for 循环只能处理列表或者是执行数据库的结果")))
-                                result))))))
+; 创建查询用户组的方法 get_user_group
+; 首先、查询缓存，如果缓存中存在，则返回
+; 如果没有查询到，就查询数据库并将查询的结果保存到，缓存中
+(defn get_user_group [ignite group_id user_token]
+    (let [vs (MyVar. (my-lexical/no-sql-get-vs ignite group_id (doto (Hashtable.) (.put "table_name" "user_group_cache") (.put "key" (my-lexical/get-value user_token)))))]
+        (cond (my-lexical/not-empty? (my-lexical/get-value vs)) (my-lexical/get-value vs)
+              :else (let [rs (MyVar. (my-smart-db/query_sql ignite group_id "select g.id, g.data_set_name, g.group_type from my_users_group as g where g.user_token = ?" [(my-lexical/to_arryList [(my-lexical/get-value user_token)])])) result (MyVar. )]
+                        (do
+                            (cond (my-lexical/my-is-iter? rs) (try
+                                                                  (loop [M-F-v1725-I-Q1726-c-Y (my-lexical/get-my-iter rs)]
+                                                                      (if (.hasNext M-F-v1725-I-Q1726-c-Y)
+                                                                          (let [r (.next M-F-v1725-I-Q1726-c-Y)]
+                                                                              (my-lexical/no-sql-insert ignite group_id (doto (Hashtable.) (.put "table_name" "user_group_cache") (.put "key" (my-lexical/get-value user_token)) (.put "value" r)))
+                                                                              (.setVar result r)
+                                                                              (recur M-F-v1725-I-Q1726-c-Y))))
+                                                                  (catch Exception e
+                                                                      (if-not (= (.getMessage e) "my-break")
+                                                                          (throw e))))
+                                  (my-lexical/my-is-seq? rs) (try
+                                                                 (doseq [r (my-lexical/get-my-seq rs)]
+                                                                     (my-lexical/no-sql-insert ignite group_id (doto (Hashtable.) (.put "table_name" "user_group_cache") (.put "key" (my-lexical/get-value user_token)) (.put "value" r)))
+                                                                     (.setVar result r))
+                                                                 (catch Exception e
+                                                                     (if-not (= (.getMessage e) "my-break")
+                                                                         (throw e))))
+                                  :else
+                                  (throw (Exception. "for 循环只能处理列表或者是执行数据库的结果")))
+                            result)))))
 
-
-(defn get_user_token [ignite group_id c_group_name_f1759]
-    (let [group_name (MyVar. (MyConvertUtil/ConvertToString c_group_name_f1759)) group_token (MyVar. )]
+;
+(defn get_user_token [ignite group_id group_name]
+    (let [group_token (MyVar. )]
         (let [rs (MyVar. (my-smart-db/query_sql ignite group_id "select m.id, m.user_token from my_users_group m where m.group_name = ?" [(my-lexical/to_arryList [(my-lexical/get-value group_name)])]))]
             (do
                 (cond (my-lexical/my-is-iter? rs) (try
@@ -104,19 +107,45 @@
                       (throw (Exception. "for 循环只能处理列表或者是执行数据库的结果")))
                 group_token))))
 
-(defn add_user_group [ignite group_id c_group_name_f1766 c_user_token_f1767 c_group_type_f1768 c_data_set_name_f1769]
-    (let [group_name (MyVar. (MyConvertUtil/ConvertToString c_group_name_f1766)) user_token (MyVar. (MyConvertUtil/ConvertToString c_user_token_f1767)) group_type (MyVar. (MyConvertUtil/ConvertToString c_group_type_f1768)) data_set_name (MyVar. (MyConvertUtil/ConvertToString c_data_set_name_f1769))]
-        (cond (my-smart-scenes/my-invoke-scenes ignite group_id "has_user_token_type" [(my-lexical/get-value group_type)]) (cond (my-lexical/not-null-or-empty? (my-smart-scenes/my-invoke-scenes ignite group_id "get_user_group" [(my-lexical/get-value user_token)])) "已经存在了该 user_token 不能重复插入！"
-                                                                                                                                 :else (let [user_group_id (MyVar. (my-lexical/auto_id ignite group_id "my_users_group"))]
-                                                                                                                                           (let [lst (MyVar. (my-lexical/to_arryList [(my-lexical/to_arryList ["insert into my_users_group (id, group_name, data_set_name, user_token, group_type) values (?, ?, ?, ?, ?)" (my-lexical/to_arryList [(my-lexical/get-value user_group_id) (my-lexical/get-value group_name) (my-lexical/get-value data_set_name) (my-lexical/get-value user_token) (my-lexical/get-value group_type)])])]))]
-                                                                                                                                               (do
-                                                                                                                                                   (my-lexical/list-add (my-lexical/get-value lst) (my-lexical/to_arryList ["insert into call_scenes (group_id, to_group_id, scenes_name) values (?, ?, ?)" (my-lexical/to_arryList [0 (my-lexical/get-value user_group_id) "get_user_group"])]))
-                                                                                                                                                   (my-smart-db/trans ignite group_id (my-lexical/get-value lst))
-                                                                                                                                                   (smart-func/smart-view ignite group_id (my-lexical/get-value group_name) (format "select * from my_meta.my_caches where dataset_name = '%s'" (my-lexical/get-value data_set_name)))
-                                                                                                                                                   (smart-func/smart-view ignite group_id (my-lexical/get-value group_name) (format "select * from my_meta.call_scenes where to_group_id = %s" (my-lexical/get-value user_group_id)))
-                                                                                                                                                   (smart-func/smart-view ignite group_id (my-lexical/get-value group_name) (format "select * from my_meta.my_cron where group_id = '%s'" (my-lexical/get-value user_group_id)))
-                                                                                                                                                   (smart-func/smart-view ignite group_id (my-lexical/get-value group_name) (format "select * from my_meta.my_delete_views where group_id = %s" (my-lexical/get-value user_group_id)))
-                                                                                                                                                   (smart-func/smart-view ignite group_id (my-lexical/get-value group_name) (format "select * from my_meta.my_insert_views where group_id = %s" (my-lexical/get-value user_group_id)))
-                                                                                                                                                   (smart-func/smart-view ignite group_id (my-lexical/get-value group_name) (format "select * from my_meta.my_update_views where group_id = %s" (my-lexical/get-value user_group_id)))
-                                                                                                                                                   (smart-func/smart-view ignite group_id (my-lexical/get-value group_name) (format "select * from my_meta.my_select_views where group_id = %s" (my-lexical/get-value user_group_id)))
-                                                                                                                                                   (smart-func/smart-view ignite group_id (my-lexical/get-value group_name) "select id, group_name, data_set_name, group_type from my_meta.my_users_group"))))))))
+(defn add_user_group [ignite group_id group_name user_token group_type data_set_name]
+    (cond (has_user_token_type ignite group_id group_type)
+          (cond (my-lexical/not-null-or-empty? (get_user_group ignite group_id (my-lexical/get-value user_token))) "已经存在了该 user_token 不能重复插入！"
+                :else (let [user_group_id (MyVar. (my-lexical/auto_id ignite group_id "my_users_group"))]
+                          (let [lst (MyVar. (my-lexical/to_arryList [(my-lexical/to_arryList ["insert into my_users_group (id, group_name, data_set_name, user_token, group_type) values (?, ?, ?, ?, ?)" (my-lexical/to_arryList [(my-lexical/get-value user_group_id) group_name (my-lexical/get-value data_set_name) (my-lexical/get-value user_token) (my-lexical/get-value group_type)])])]))]
+                              (do
+                                  (my-lexical/list-add (my-lexical/get-value lst) (my-lexical/to_arryList ["insert into call_scenes (group_id, to_group_id, scenes_name) values (?, ?, ?)" (my-lexical/to_arryList [0 (my-lexical/get-value user_group_id) "get_user_group"])]))
+                                  (my-smart-db/trans ignite group_id (my-lexical/get-value lst))
+                                  (smart-func/smart-view ignite group_id group_name (format "select * from my_meta.my_caches where dataset_name = '%s'" (my-lexical/get-value data_set_name)))
+                                  (smart-func/smart-view ignite group_id group_name (format "select * from my_meta.call_scenes where to_group_id = %s" (my-lexical/get-value user_group_id)))
+                                  (smart-func/smart-view ignite group_id group_name (format "select * from my_meta.my_cron where group_id = '%s'" (my-lexical/get-value user_group_id)))
+                                  (smart-func/smart-view ignite group_id group_name (format "select * from my_meta.my_delete_views where group_id = %s" (my-lexical/get-value user_group_id)))
+                                  (smart-func/smart-view ignite group_id group_name (format "select * from my_meta.my_insert_views where group_id = %s" (my-lexical/get-value user_group_id)))
+                                  (smart-func/smart-view ignite group_id group_name (format "select * from my_meta.my_update_views where group_id = %s" (my-lexical/get-value user_group_id)))
+                                  (smart-func/smart-view ignite group_id group_name (format "select * from my_meta.my_select_views where group_id = %s" (my-lexical/get-value user_group_id)))
+                                  (smart-func/smart-view ignite group_id group_name "select id, group_name, data_set_name, group_type from my_meta.my_users_group")))))))
+
+(defn update_user_group [ignite group_id group_name group_type]
+    (let [user_token (MyVar. (get_user_token ignite group_id (my-lexical/get-value group_name)))]
+        (let [vs (MyVar. (my-lexical/no-sql-get-vs ignite group_id (doto (Hashtable.) (.put "table_name" "user_group_cache") (.put "key" (last (my-lexical/get-value user_token))))))]
+            (cond (nil? (my-lexical/get-value vs)) (my-smart-db/query_sql ignite group_id "update my_users_group set group_type = ? where group_name = ?" [(my-lexical/to_arryList [(my-lexical/get-value group_type) (my-lexical/get-value group_name)])])
+                  :else (let [new_vs (MyVar. (my-lexical/list-set (my-lexical/get-value vs) 2 (my-lexical/get-value group_type))) lst (MyVar. (my-lexical/to_arryList [(my-lexical/to_arryList ["update my_users_group set group_type = ? where group_name = ?" (my-lexical/to_arryList [(my-lexical/get-value group_type) (my-lexical/get-value group_name)])])]))]
+                            (do
+                                (my-lexical/list-add (my-lexical/get-value lst) (my-lexical/to_arryList [(my-lexical/no-sql-update-tran ignite group_id (doto (Hashtable.) (.put "table_name" "user_group_cache") (.put "key" (last (my-lexical/get-value user_token))) (.put "value" (my-lexical/get-value new_vs))))]))
+                                (my-smart-db/trans ignite group_id (my-lexical/get-value lst))))))))
+
+
+(defn delete_user_group [ignite group_id group_name]
+    (let [user_token (MyVar. (get_user_token ignite group_id (my-lexical/get-value group_name)))]
+        (do
+            (smart-func/rm-smart-view ignite group_id group_name "my_meta.my_caches" "select")
+            (smart-func/rm-smart-view ignite group_id group_name "my_meta.call_scenes" "select")
+            (smart-func/rm-smart-view ignite group_id group_name "my_meta.my_cron" "select")
+            (smart-func/rm-smart-view ignite group_id group_name "my_meta.my_delete_views" "select")
+            (smart-func/rm-smart-view ignite group_id group_name "my_meta.my_insert_views" "select")
+            (smart-func/rm-smart-view ignite group_id group_name "my_meta.my_update_views" "select")
+            (smart-func/rm-smart-view ignite group_id group_name "my_meta.my_select_views" "select")
+            (smart-func/rm-smart-view ignite group_id group_name "my_meta.my_users_group" "select")
+            (let [lst (MyVar. (my-lexical/to_arryList [(my-lexical/to_arryList ["delete from my_users_group where group_name = ?" (my-lexical/to_arryList [(my-lexical/get-value group_name)])])]))]
+                (my-lexical/list-add (my-lexical/get-value lst) (my-lexical/to_arryList ["delete from call_scenes where to_group_id = ?" (my-lexical/to_arryList [(first (my-lexical/get-value user_token))])]))
+                (my-lexical/list-add (my-lexical/get-value lst) (my-lexical/no-sql-delete-tran ignite group_id (doto (Hashtable.) (.put "table_name" "user_group_cache") (.put "key" (last (my-lexical/get-value user_token))))))
+                (my-smart-db/trans ignite group_id (my-lexical/get-value lst))))))
